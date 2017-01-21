a9b58bc7ffb30f1ddce0cafa82531d7aaf70fa14
Maxim <maxim_st@gmx.de>
2017-01-21 20:44:53 +0100

    functioning generate button

--- Node.js
+++ Node.js
@@ -13,9 +13,11 @@ function node(x,y){
 			if(nodes[i].x === this.x && nodes[i].y === this.y){
 				continue;
 			}else{
+				context.beginPath();
 				context.moveTo(this.x, this.y);
 				context.lineTo(nodes[i].x,nodes[i].y);
 				context.stroke();
+				context.closePath();
 			}
 		}
 	}
@@ -26,12 +28,13 @@ function generateNode(){
 	nodeNum ++;
 }
 
-for(var i = 0; i < 10; i++){
-	generateNode();
-}
-
 function update(){
 	context.clearRect(0,0,canvas.width,canvas.height);
+	nodes = [];
+	nodeNum = 0;
+	for(var i = 0; i < 10; i++){
+		generateNode();
+	}
 	for(var i = 0; i < nodes.length; i++){
 		nodes[i].draw();
 	}
